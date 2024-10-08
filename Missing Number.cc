7ce6737b8ce3a80c43ff2b3f02891491bd144d1c
Sk Tasnim Ur Rahman <162748135+sktasnim2005@users.noreply.github.com>
2024-10-08 12:14:31 +0600

    Create Missing Number.cc

---
+++ Missing Number.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int sum;
+    cin >> sum;
+    int a,b,c,d;
+    cin >> a >> b >> c;
+    d=sum-a-b-c;
+    cout<<d;
+    return 0;
+}
