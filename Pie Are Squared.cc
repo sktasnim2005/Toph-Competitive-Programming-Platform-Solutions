c320cfb9ccb25f0967884ba6c0cc805f0abda9d8
Sk Tasnim Ur Rahman <162748135+sktasnim2005@users.noreply.github.com>
2024-10-08 11:56:53 +0600

    Create Pie Are Squared.cc

---
+++ Pie Are Squared.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int r;
+    cin >> r;
+    double x= 3.141592653589793*r*r;
+    cout<<fixed<<setprecision(12)<<x;
+    return 0;
+}
