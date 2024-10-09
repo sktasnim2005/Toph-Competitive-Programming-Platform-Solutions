f357222752a1335366d93235e5b8a5377fc54983
Sk Tasnim Ur Rahman <162748135+sktasnim2005@users.noreply.github.com>
2024-10-09 19:10:40 +0600

    Create Tricky Ratio.cc

---
+++ Tricky Ratio.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    float r;
+    cin>>r;
+    float d=2*r;
+    float C=2*3.14159*r;
+    float x=C/d;
+    cout<<fixed<<setprecision(5)<<x;
+    return 0;
+}
