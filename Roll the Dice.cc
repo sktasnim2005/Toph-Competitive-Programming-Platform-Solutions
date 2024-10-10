a98739ac48b2dbee810b920f82da2fa6c0c10939
Sk Tasnim Ur Rahman <162748135+sktasnim2005@users.noreply.github.com>
2024-10-10 22:57:45 +0600

    Create Roll the Dice.cc

---
+++ Roll the Dice.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n;
+        cin >> n;
+        long long max_n=n*6;
+
+        cout << max_n<<" "<< n << endl;
+    }
+    return 0;
+}
