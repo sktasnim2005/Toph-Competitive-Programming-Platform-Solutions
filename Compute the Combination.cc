158dc9a43d0f0e6ba5aad553ebcadb12b8fe1a1a
Sk Tasnim Ur Rahman <162748135+sktasnim2005@users.noreply.github.com>
2024-10-10 15:26:54 +0600

    Create Compute the Combination.cc

---
+++ Compute the Combination.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+      int n;
+      cin>>n;
+      if(n==0) cout<<1<<endl;
+      else cout<<0<<endl;
+      }
+	return 0;
+}
