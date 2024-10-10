70a5bff60e787b317441c2d51d3198a719aacb69
Sk Tasnim Ur Rahman <162748135+sktasnim2005@users.noreply.github.com>
2024-10-10 21:41:47 +0600

    Create The Titan.cc

---
+++ The Titan.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    long long n;
+    cin >> n;
+
+    long long result = n*(n+1)*(2*n +1) /6 ;
+
+
+    cout << result << endl;
+    return 0;
+}
